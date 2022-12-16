fe548cc7efcc79c952575d1ebfefae02603c1bbf
lil-dev16 <olusegunkd2017@gmail.com>
2022-12-16 12:12:31 +0100

    n

--- first.js
+++ first.js
@@ -1,6 +1,10 @@
 const http = require('http');
+const date = require('./myfirstmodule');
+const fs = require('fs');
+
 http.createServer((req,res)=> {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello world')
+    res.write('todays date is:' + date.myDateTime())
+    res.end();
 
 }).listen(8080);
